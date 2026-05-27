ce3fb0600f67dc4c8a4597045f9fe869bb027075
Prince-Web-Developer <268293963+Prince-Web-Developer@users.noreply.github.com>
2026-05-27 10:19:57 +0500

    Setup intial project structure

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff2|woff)$/i,
+                type: "asset/resource"
+            }
+        ],
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dist"),
+        clean: true,
+    },
+};
